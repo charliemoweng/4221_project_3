a210bccb1defc5644db082d930ea750c5a920a86
Angie <angielimenqi7@gmail.com>
2024-03-28 14:10:29 +0800

    Alerts showing current state of the game

--- findkey/src/sections/arena_section/index.tsx
+++ findkey/src/sections/arena_section/index.tsx
@@ -3,15 +3,25 @@ import {
     GameInfoContext,
     GameInfoContextType,
 } from "../../hooks/GameInfoProvider";
-import { Button } from "@mui/material";
+import { Button, Alert } from "@mui/material";
 import MonsterSelectionButton from "./components/monsterSelectionButton";
 import Team from "./components/team";
+import { GiShardSword, GiNextButton } from "react-icons/gi";
 
 type Props = {};
+
+enum AlertStates {
+    NONE,
+    NO_MONSTERS_PICKED,
+    TEAM_PICKED_BEFORE,
+    WIN,
+    LOSE,
+}
 
 const ArenaSection = (props: Props) => {
     const [currTeam, setCurrTeam] = useState<Set<string>>(new Set<string>());
     const [isSelectingTeam, setIsSelectingTeam] = useState<boolean>(true);
+    const [alertState, setAlertState] = useState<AlertStates>(AlertStates.NONE);
 
     // to keep track of values after fighting
     const [enemiesDead, setEnemiesDead] = useState<Set<string>>(
@@ -36,16 +46,23 @@ const ArenaSection = (props: Props) => {
 
     const handleFight = () => {
         // check if it is empty
-        if (currTeam.size === 0) return;
+        if (currTeam.size === 0) {
+            setAlertState(AlertStates.NO_MONSTERS_PICKED);
+            return;
+        }
 
         // check whether set already exists
-        if (wasTeamUsedBefore(currTeam)) return;
+        if (wasTeamUsedBefore(currTeam)) {
+            setAlertState(AlertStates.TEAM_PICKED_BEFORE);
+            return;
+        }
 
         // get closure and whether all the enemies were wiped out
         setEnemiesDead(fightOpponent(currTeam));
 
         // if enemies dead size is the number of attributes, it means the enemy team got wiped, player won
         setIsWinRound(enemiesDead.size === matchInfo?.noOfAttributes);
+        setAlertState(isWinRound ? AlertStates.WIN : AlertStates.LOSE);
 
         //change fight button to next round button
         setIsSelectingTeam(false);
@@ -57,6 +74,7 @@ const ArenaSection = (props: Props) => {
         setEnemiesDead(new Set<string>());
         setIsSelectingTeam(true);
         setIsWinRound(false);
+        setAlertState(AlertStates.NONE);
 
         // start next round
         handleNewRound();
@@ -73,6 +91,41 @@ const ArenaSection = (props: Props) => {
 
         return monsters;
     }, [matchInfo?.noOfAttributes]);
+
+    // if player could not wipe enemy team, it means their team got wiped
+    const isPlayerTeamWiped = () => {
+        return !isSelectingTeam && !isWinRound;
+    };
+
+    const renderAlerts = () => {
+        return (
+            <div style={styles.popup}>
+                {alertState === AlertStates.NO_MONSTERS_PICKED && (
+                    <Alert severity="error">
+                        Your team is empty! You need to select at least 1
+                        monster!
+                    </Alert>
+                )}
+                {alertState === AlertStates.TEAM_PICKED_BEFORE && (
+                    <Alert severity="warning">
+                        You already selected this combination of monsters! Pick
+                        a different combination.
+                    </Alert>
+                )}
+                {alertState === AlertStates.WIN && (
+                    <Alert severity="success">
+                        Successfully defeated the enemy's team!
+                    </Alert>
+                )}
+                {alertState === AlertStates.LOSE && (
+                    <Alert severity="warning">
+                        You were unable to full wipe the enemy's team and lost
+                        this round...
+                    </Alert>
+                )}
+            </div>
+        );
+    };
 
     const renderMonsterSelectionSection = useMemo(() => {
         const buttons: any[] = [];
@@ -93,13 +146,8 @@ const ArenaSection = (props: Props) => {
             );
         });
 
-        return <div style={styles.selectionContainer}>{buttons}</div>;
+        return <div style={styles.selectionButtonContainer}>{buttons}</div>;
     }, [monsterTypes, currTeam, isSelectingTeam]);
-
-    // if player could not wipe enemy team, it means their team got wiped
-    const isPlayerTeamWiped = () => {
-        return !isSelectingTeam && !isWinRound;
-    };
 
     return (
         <div style={styles.container}>
@@ -123,16 +171,29 @@ const ArenaSection = (props: Props) => {
                     deadMonsters={enemiesDead}
                 />
             </div>
-            {renderMonsterSelectionSection}
-            {isSelectingTeam ? (
-                <Button variant="contained" onClick={handleFight}>
-                    Fight
-                </Button>
-            ) : (
-                <Button variant="contained" onClick={startNextRound}>
-                    Next Round
-                </Button>
-            )}
+            <div style={styles.selectionContainer}>
+                {renderMonsterSelectionSection}
+                {isSelectingTeam ? (
+                    <Button
+                        variant="contained"
+                        onClick={handleFight}
+                        sx={[styles.fightButton, styles.button]}
+                    >
+                        <GiShardSword size={40} />
+                        Fight
+                    </Button>
+                ) : (
+                    <Button
+                        variant="contained"
+                        onClick={startNextRound}
+                        sx={[styles.nextRoundButton, styles.button]}
+                    >
+                        <GiNextButton />
+                        Next Round
+                    </Button>
+                )}
+                {renderAlerts()}
+            </div>
         </div>
     );
 };
@@ -141,15 +202,7 @@ const styles: any = {
     container: {
         display: "flex",
         flexDirection: "column",
-    },
-    selectionContainer: {
-        display: "flex",
-        flexDirection: "row",
         justifyContent: "center",
-    },
-    selectionButton: {
-        height: "100px",
-        width: "100px",
     },
     arenaContainer: {
         display: "flex",
@@ -158,6 +211,42 @@ const styles: any = {
         borderWidth: "3px",
         borderColor: "black",
         margin: "70px",
+    },
+    selectionButtonContainer: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+    },
+    selectionButton: {
+        height: "100px",
+        width: "100px",
+    },
+    selectionContainer: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    button: {
+        marginTop: "30px",
+        width: "150px",
+        height: "50px",
+        justifyContent: "space-evenly",
+    },
+    fightButton: {
+        backgroundColor: "#FF8C00",
+        ":hover": {
+            backgroundColor: "#E55A00",
+        },
+    },
+    nextRoundButton: {
+        backgroundColor: "#007DFF",
+        ":hover": {
+            backgroundColor: "#0060C3",
+        },
+    },
+    popup: {
+        marginTop: "30px",
+        width: "600px",
     },
 };
 
